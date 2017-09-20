ea78b76c0d02617bc0a2f874a87d6d682cd0a375
Ricardo Dias Cavalcante <ricardo.cavalcante@tst.jus.br>
2017-09-20 19:25:07 -0300

    Testing encoding

--- src/index.js
+++ src/index.js
@@ -1,17 +1,18 @@
 import express from 'express';
 import nodehun from 'nodehun';
 
-
+import SpellChecker from './spellchecker';
 
 
 const app = express();
+const PORT = process.env.PORT || 3000;
 
 app.get('/check/:word',(req,resp,next)=>{
-    console.log('teste');
+
     resp.write("Hellp");
     resp.end();
 });
 
 app.listen(8080,()=>{
-    console.log('started on 8080');
+    console.log(`Spell checker started on ${PORT}`);
 });
--- src/spellchecker.js
+++ src/spellchecker.js
@@ -1,25 +1,44 @@
 import nodehun from 'nodehun';
 import fs from 'fs';
+import iconv from 'iconv-lite';
+
+const encoding = require('encoding');
 
 class SpellChecker {
     constructor(){
-        let affBuff = fs.readFileSync(__dirname+'/dic/hunspell/pt_BR.aff');
-        let dicBuff = fs.readFileSync(__dirname+'/dic/hunspell/pt_BR.dic');
+        let affBuff = fs.readFileSync(__dirname+'/dic/hunspell/utf/pt_BR.aff');
+        let dicBuff = fs.readFileSync(__dirname+'/dic/hunspell/utf/pt_BR.dic');
         this.dict = new nodehun(affBuff,dicBuff);
 
     }
 
-    checkWord(word){
+    check(word){
         return  new Promise((resolve,reject)=>{
             this.dict.spellSuggest(word,(err,correct, sugestion, originalWord)=>{
                 if(err){
                     return reject(err);
                 }
-                return resolve({
+                try{
+                    let s = sugestion;
+                    console.log(encoding.convert(s,'win1252','utf8').toString());
+
+                    
+
+                    console.log(s);
+    
+                }catch(e){
+                    console.log(e);
+                    return reject(err);
+                }
+                
+                
+                let result = {
                     correct: correct,
-                    sugestion: sugestion,
+                    sugestion: sugestion.map((element) =>  iconv.decode(new Buffer(element),'win1251')),
                     originalWord: originalWord
-                })
+                }
+               
+                return resolve(result);
             });
 
         });
